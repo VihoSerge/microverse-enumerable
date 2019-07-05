3ec3156c8b024c24da8a94e01881392dd9f7f3db
VihoSerge <komi.vihoserge@gmail.com>
2019-07-05 18:10:03 +0000

    #my_any method

--- enumerable.rb
+++ enumerable.rb
@@ -30,7 +30,12 @@ module Enumerable
   end
 
   def my_any?
+    self.my_each do |i|
+      block = yield(i) if block_given?
+      return true if i.nil? || i || block
+    end
 
+    false
   end
 
   def my_none?
@@ -42,10 +47,35 @@ module Enumerable
   end
 
   def my_map
+    arr = []
+    self.my_each do |i|
+      arr << yield(i)
+    end
 
+    arr
   end
 
   def my_inject
 
   end
 end
+
+arr = [1, 3]
+# puts arr.my_each
+# arr.my_each_with_index do |val,i|
+#   puts val, i
+# end
+
+# puts [1,2,3,4,5,6].my_select { |num| num.even?  }
+
+# puts [1, 3].my_map { |i| i*i }
+
+# puts [nil, true, 99].all?
+# puts [nil, true, 99].my_all?
+# puts [].my_all?(2, 34)
+
+puts %w[ant bear cat].my_any? { |word| word.length >= 3 } #=> true
+puts %w[ant bear cat].my_any? { |word| word.length >= 4 } #=> false
+                     #=> true
+puts [nil, true, 99].my_any?                              #=> false
+puts [].my_any?
